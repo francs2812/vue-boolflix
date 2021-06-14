205778be255f800c850f6a2c4fcacc445744c5b7
francs2812 <emiliofrangella2812@gmail.com>
2021-06-14 17:20:29 +0200

    ricerca api

---
+++ src/components/Header.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="d-flex justify-content-around align-items-center"> 
+    <a href="https://fontmeme.com/netflix-font/"><img src="https://fontmeme.com/permalink/210614/dbe2da256cf873b5e39378a2ee64c904.png" alt="netflix-font" border="0"></a>
+    <div>
+        <input  onkeyup="getUser" v-model="serch" type="text" name="title" id="title">
+        <input @click="movieSearch" type="button" value="search">
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+    name: 'Header',
+    data: function(){
+        return {
+              serch:"",
+              url:"https://api.themoviedb.org/3/search/movie" , 
+              film:""
+        }
+    },
+    // created: function(){
+    //  axios
+    //         .get(this.url ,{ 
+    //                 params:{
+    //                   api_key : "021c1d61b0a3435190dbf0a9fc20b605",
+    //                   language : "it-IT",
+    //                   query : this.serch,
+    //                 }
+    //         })
+    //         .then( 
+    //             (response) => {
+    //                 console.log(response);
+    //                 this.film = response.data.results;
+    //                 console.log( this.film);
+    //         }
+    //        )
+    //         .catch();
+    //         }, 
+    methods:{
+       movieSearch() {
+            axios.get(this.url ,{ 
+                    params:{
+                      api_key : "021c1d61b0a3435190dbf0a9fc20b605",
+                      language : "it-IT",
+                      query : this.serch,
+                    }
+            })
+            .then( 
+                (response) => {
+                    //console.log(response);
+                    this.film = response.data.results;
+                    //console.log( this.film);
+            })
+            }
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+
+</style>
